1c2926e44b75c7b5105be90d8bdd548540aa73e3
avebrecla <avebrecla@gmail.com>
2025-08-20 22:57:28 -0400

    - attempting to fix form

--- components/feedback-form.jsx
+++ components/feedback-form.jsx
@@ -36,7 +36,7 @@ export function FeedbackForm() {
         <div className="w-full md:max-w-md">
             <Card title="what would u like to see?">
                 <form name="feedback" onSubmit={handleFormSubmit} className="flex flex-col gap-3 align-center">
-                    <input type="hidden" name="form-name" value="what would u like to see?" />
+                    <input type="hidden" name="form-name" value="feedback" />
                     <input name="favorite color" type="text" placeholder="favorite color (just cause)" required className="input" />
                     <input name="message" type="text" placeholder="recs" required className="input" />
                     <button className="btn" type="submit" disabled={status === 'pending'}>
